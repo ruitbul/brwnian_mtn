247aa73d30b4527ea2a9d9845f43cb03755738dd
ruitbull <robbybullock@icloud.com>
2021-03-11 18:09:33 -0800

    Fifth Commit Motion Working

--- sketch.js
+++ sketch.js
@@ -1,20 +1,28 @@
 let birds = [];
-//let size = 1;
+
+let x;
+
+let y;
+
+let timer = 0;
 
 function preload() { 
   img = loadImage("mtn.jpg"); 
 } 
 
 function setup() {
-  createCanvas(700, 900); 
-  frameRate(30);
+  createCanvas(600, 700); 
+
+  // x = random(width);
+  // y = random(height);
+  
   for (let i = 0; i < 8; i++) {
     birds.push(new Motion());
   }
 }
 
 function draw() {
-  image(img, 20, 40, 700, 900);
+  image(img, 0, 0, 600, 700);
   for (let i = 0; i < birds.length; i++) {
     birds[i].move();
     birds[i].display();
@@ -28,6 +36,8 @@ class Motion {
     this.y = random(height);
     this.diameter = random(10, 30);
     this.speed = 1;
+    // this.newX = 0;
+    // this.newY = 0;
   }
 
   move() {
@@ -38,17 +48,25 @@ class Motion {
   display() {
     noFill();
     stroke(255, 151, 112);
+    
+    if(timer < 3000){
+      timer ++;
+      }
+
+      let newX = map(timer, 0, 3000, this.x, width/2+ random(20)); //+ random(10));
+      let newY = map(timer, 0, 3000, this.y, height/2 + random(20));//+random(10));
+    
     beginShape();
     for (let i=0; i<1500; i++) {
       
       
-      vertex(this.x, this.y);
+      vertex(newX, newY);
       
       
 
       
-      this.x += random(-.5, .5);
-      this.y += random(-.5, .5);
+      newX += random(-.5, .5);
+      newY += random(-.5, .5);
     }
     endShape();
   }
